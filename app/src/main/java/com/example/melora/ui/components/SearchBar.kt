db80201345900e3774faa270c5a64dadaa478d91
LUKAS <ldonoso472@gmail.com>
2025-10-20 18:53:59 -0300

    Creacion xml

--- app/src/main/java/com/example/melora/ui/components/SearchBar.kt
+++ app/src/main/java/com/example/melora/ui/components/SearchBar.kt
@@ -1,54 +1,101 @@
 package com.example.melora.ui.components
 
-
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.melora.data.local.song.SongEntity
+import com.example.melora.ui.screen.SongItem
+import com.example.melora.viewmodel.SearchViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchBar(
-    query: String,
-    onQueryChange: (String) -> Unit, //preguntar
-    onSearch: () -> Unit //preguntar
-){
-    val focusManager = LocalFocusManager.current
+fun SimpleSearchBar(
+    textFieldState: TextFieldState,
+    onSearch: (String) -> Unit,
+    searchResults: List<String>,
+    modifier: Modifier = Modifier,
 
-    OutlinedTextField(
-        value = query,
-        onValueChange = {onQueryChange(it) }, //preguntar
+) {
+    val vm : SearchViewModel = viewModel()
+    // Controls expansion state of the search bar
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    val songs by vm.songs.collectAsStateWithLifecycle()
+
+    Box(
+        modifier
+            .fillMaxSize()
+    ) {
+        SearchBar(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .semantics { traversalIndex = 0f },
+            inputField = {
+                SearchBarDefaults.InputField(
+                    query = textFieldState.text.toString(),
+                    onQueryChange = { textFieldState.edit { replace(0, length, it) } },
+                    onSearch = {
+                        onSearch(textFieldState.text.toString())
+                        expanded = false
+                    },
+                    expanded = expanded,
+                    onExpandedChange = { expanded = it },
+                    placeholder = { Text("Search") }
+                )
+            },
+            expanded = expanded,
+            onExpandedChange = { expanded = it },
+        ) {
+            // Display search results in a scrollable column
+            Column(Modifier.verticalScroll(rememberScrollState())) {
+                searchResults.forEach { result ->
+                    LazyColumn (modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp)
+                    ){
+                        items(songs){
+                                song ->
+                            SongItem(song = song)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun SongItem(song: SongEntity) {
+    Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 8.dp),
-        placeholder = { Text("Search...") },
-        leadingIcon = { Icon(Icons.Filled.Search,"Search Icon") },
-        trailingIcon = {
-            if(query.isNotEmpty()){
-                Icon(imageVector = Icons.Default.Clear,"Clear", modifier = Modifier.clickable{onQueryChange("")})
-            }
-        },
-        singleLine = true,
-        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
-        keyboardActions = KeyboardActions(
-            onSearch = {
-                focusManager.clearFocus()
-                onSearch()
-            }
+            .padding(8.dp)
+    ) {
+        Text(
+            text = song.songName,
+            color = Color.White
         )
-    )
+    }
 }
---
+++ app/src/main/java/com/example/melora/viewmodel/SearchViewModelFactory.kt
@@ -0,0 +1,4 @@
+package com.example.melora.viewmodel
+
+class SearchViewModelFactory {
+}
